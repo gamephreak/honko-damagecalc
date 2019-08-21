ae9ce6223e9e338c29bca1aa7df90fbfccee8f8c
Kirk Scheibelhut <kjs@scheibo.com>
2019-08-20 18:51:24 -0700

    Get calc working

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -8,7 +8,7 @@ select2($);
 import 'select2/dist/css/select2.css';
 
 import { Sprites, Icons } from './img';
-//import * as calc from 'calc';
+import * as calc from 'calc';
 
 console.log(Sprites.forBattle('Charizard-Mega-Y'));
 console.log(Sprites.forTeambuilder('Charizard-Mega-X'));
@@ -16,10 +16,10 @@ console.log(Icons.getPokemon('Vaporeon'));
 console.log(Icons.getItem('Life Orb'));
 console.log(Icons.getType('Ghost'));
 
-$('#display').select2();
-//calc.calculate(
-//1,
-//new calc.Pokemon(1, 'Gengar'),
-//new calc.Pokemon(1, 'Chansey'),
-//new calc.Move(1, 'Thunderbolt')
-//)
+//$('#display').select2();
+$('#display').html(calc.calculate(
+  1,
+  new calc.Pokemon(1, 'Gengar'),
+  new calc.Pokemon(1, 'Chansey'),
+  new calc.Move(1, 'Thunderbolt')
+).desc());
