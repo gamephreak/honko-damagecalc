8a64a1402cab736fce8c09cc1f4398da93d11218
Kirk Scheibelhut <kjs@scheibo.com>
2019-08-24 16:43:30 -0700

    Formats list

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -16,25 +16,91 @@ console.log(Icons.getPokemon('Vaporeon'));
 console.log(Icons.getItem('Life Orb'));
 console.log(Icons.getType('Ghost'));
 
-// TODO: discover programmactically based on which formats have strategies
-// TODO sort by usage stats battle numbers
-// TODO Random / Random Doubles
-const FORMATS = [
-    ['Uber', 'OU', 'UU', 'LC'],
-    ['Uber', 'OU', 'UU', 'NU', 'LC'],
-    ['Uber', 'OU', 'UU', 'NU', 'LC'],
-    ['Uber', 'OU', 'UU', 'NU', 'LC'],
-     // DW OU, VGC 11/12
-    ['Uber', 'OU', 'UU', 'RU', 'NU', 'LC', 'Doubles'],
-    // AAA, MC, STABmons, VGC 14/15/16, Mix and Mega
-    ['Uber', 'OU', 'UU', 'RU', 'NU', 'PU', 'LC', 'Monotype', 'Doubles', 'AG', 'BSS', 'BSD', 'BH', 'CAP' ],
-      // LGPE OU, AAA, MC, STABmons, VGC 17/18/19, Mix and Mega
-    ['Uber', 'OU', 'UU', 'RU', 'NU', 'PU', 'LC', 'Monotype', 'Doubles', 'AG', 'BSS', 'BSD', 'BH', 'CAP', '1v1', 'ZU' ],
+type Format = keyof typeof FORMATS;
+
+const FORMATS = {
+  Random: 1,
+  Monotype: 2,
+  AG: 3,
+  Uber: 4,
+  OU: 5,
+  UU: 6,
+  RU: 7,
+  NU: 8,
+  PU: 9,
+  ZU: 10,
+  LC: 11,
+  Doubles: 12,
+  'VGC 19': 13,
+  'VGC 18': 14,
+  'VGC 17': 15,
+  'VGC 16': 16,
+  'VGC 15': 17,
+  'VGC 14': 18,
+  'VGC 12': 19,
+  'VGC 11': 20,
+  BSS: 21,
+  BSD: 22,
+  BH: 23,
+  '1v1': 24,
+  'LGPE OU': 25,
+  CAP: 26,
+};
+
+const SUPPORTED: Format[][] = [
+  [],
+  ['Uber', 'OU', 'UU', 'LC'],
+  ['Uber', 'OU', 'UU', 'NU', 'LC'],
+  ['Uber', 'OU', 'UU', 'NU', 'LC'],
+  ['Uber', 'OU', 'UU', 'NU', 'LC'],
+  ['Uber', 'OU', 'UU', 'RU', 'NU', 'LC', 'Doubles' /*, 'VGC 12', 'VGC 11' */],
+  [
+    'Monotype',
+    'AG',
+    'Uber',
+    'OU',
+    'UU',
+    'RU',
+    'NU',
+    'PU',
+    'LC',
+    'Doubles',
+    // 'VGC 16'
+    // 'VGC 15'
+    // 'VGC 14'
+    'BSS',
+    'BSD',
+    'BH',
+    'CAP',
+  ],
+  [
+    // 'Random'
+    'Monotype',
+    'AG',
+    'Uber',
+    'OU',
+    'UU',
+    'RU',
+    'NU',
+    'PU',
+    // 'ZU'
+    'LC',
+    'Doubles',
+    // 'VGC 19'
+    // 'VGC 18'
+    // 'VGC 17'
+    'BSS',
+    'BSD',
+    'BH',
+    '1v1',
+    // 'LGPE OU'
+    'CAP',
+  ],
 ];
 
 $('#format').select2({
-    data: FORMATS[6].map((text, id) => ({id, text, selected: text === 'OU'})),
-    width: '10em',
+  data: SUPPORTED[7].map((text, id) => ({ id, text, selected: text === 'OU' })),
+  width: '10em',
 });
 // $('#display').after('<span>' + calc.calculate(
 //   1,
